e461806cac1f16f744687a024ac84ab4a637d20f
ftrocchi <ftrocchi@yahoo.com>
2014-04-19 17:59:01 -0400

    I2C_Master runs dome motor now
    Uses SendOnlySoftwareSerial.  Also, added the sabertooth library

---
+++ Source Code/I2C_Master/I2C_Common.h
@@ -0,0 +1,13 @@
+#ifndef __I2C_COMMON_H__
+#define __I2C_COMMON_H__
+
+struct I2C_DeviceAddress
+{
+    enum Value
+    {
+        PS2 = 110
+    };
+};
+
+#endif
+
--- Source Code/I2C_Master/I2C_Master.ino
+++ Source Code/I2C_Master/I2C_Master.ino
@@ -1,9 +1,42 @@
+#include <Wire.h>
+#include <Sabertooth.h>
+#include <SendOnlySoftwareSerial.h>
+#include "I2C_Common.h"
 
+SendOnlySoftwareSerial syren(11);
+Sabertooth ST(128, syren);
+
+#define MAX_ARRAY_LENGTH 20
+byte gamePadState[MAX_ARRAY_LENGTH];
 
 void setup()
 {
+    syren.begin(9600);
+    ST.autobaud();
+
+    Wire.begin();
 }
 
 void loop()
 {
+    ProcessPS2();
+    
+    // take the left stick x axis value / 2 and send to the motor
+    int motorValue = gamePadState[0] - 128;
+    
+    ST.motor(motorValue);
+
+    delay(20);
+}
+
+void ProcessPS2()
+{
+    Wire.requestFrom(I2C_DeviceAddress::PS2, 20);
+    
+    int index=0;
+    while (Wire.available())
+    {
+        gamePadState[index] = Wire.read();
+        index++;
+    }
 }
