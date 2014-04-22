8ce0b8079ce5a2fa52b32795ed24214c8a8da3c9
ftrocchi <ftrocchi@yahoo.com>
2014-04-22 08:15:24 -0400

    Sync'd I2C_Common.h

--- Source Code/I2C_Master/I2C_Common.h
+++ Source Code/I2C_Master/I2C_Common.h
@@ -5,7 +5,19 @@ struct I2C_DeviceAddress
 {
     enum Value
     {
-        PS2 = 110
+        PS2 = 110,
+        WAV = 111
+    };
+};
+
+struct I2C_WAV_Command
+{
+    enum Value
+    {
+        PlaySolo = 0,
+        PlayPoly = 1,
+        StopAll = 2,
+        SetMasterVolume = 3
     };
 };
 
--- Source Code/I2C_PS2/I2C_Common.h
+++ Source Code/I2C_PS2/I2C_Common.h
@@ -5,8 +5,21 @@ struct I2C_DeviceAddress
 {
     enum Value
     {
-        PS2 = 110
+        PS2 = 110,
+        WAV = 111
+    };
+};
+
+struct I2C_WAV_Command
+{
+    enum Value
+    {
+        PlaySolo = 0,
+        PlayPoly = 1,
+        StopAll = 2,
+        SetMasterVolume = 3
     };
 };
 
 #endif
+
