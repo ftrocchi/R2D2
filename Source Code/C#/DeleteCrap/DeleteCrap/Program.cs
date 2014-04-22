04ead4536b7db1d6cbcfb00c36d6a3c93efd08bf
ftrocchi <ftrocchi@yahoo.com>
2014-04-22 10:27:37 -0400

    Added file sync utility
    Moved source code around

---
+++ Source Code/C#/DeleteCrap/DeleteCrap/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DeleteCrap
+{
+    class Program
+    {
+        static void Main()
+        {
+            string drive = FindCorrectDrive();
+
+            if (!string.IsNullOrEmpty(drive))
+                DeleteCrapFiles(drive);
+
+            Console.WriteLine("ALL DONE");
+            Console.ReadLine();
+        }
+
+        private static void DeleteCrapFiles(string drive)
+        {
+            var dir = new DirectoryInfo(drive);
+            dir.GetFiles(".*", SearchOption.AllDirectories).ToList().ForEach(f => { Console.WriteLine("DELETING FILE: " + f.Name); f.Delete(); });
+            dir.GetDirectories(".*", SearchOption.AllDirectories).ToList().ForEach(d => { Console.WriteLine("DELETING DIR : " + d.Name); d.Delete(true); });
+        }
+
+        private static string FindCorrectDrive()
+        {
+            string result = string.Empty;
+            
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo d in allDrives)
+                if (d.IsReady && d.VolumeLabel == "R2D2 FILES")
+                    result = d.Name;
+
+            return result;
+        }
+    }
+}
---
+++ Source Code/C#/SyncCommonFiles/SyncCommonFiles/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace SyncCommonFiles
+{
+    class Program
+    {
+        static string sourceFile;
+        static DateTime sourceFileDate;
+
+        static void Main()
+        {
+            SearchForSourceFile("I2C_Common.h", Directory.GetCurrentDirectory());
+
+            Console.WriteLine(sourceFile);
+            Console.WriteLine(sourceFileDate);
+
+            SearchForSourceFile("I2C_Common.h", Directory.GetCurrentDirectory(), sourceFile);
+
+            Console.WriteLine("ALL DONE");
+            Console.ReadLine();
+        }
+
+        private static void SearchForSourceFile(string fileNameToFind, string directoryToSearch, string source = "NONE")
+        {
+            foreach (string d in Directory.GetDirectories(directoryToSearch))
+            {
+                foreach (string f in Directory.GetFiles(d, fileNameToFind))
+                {
+                    if (source == "NONE")
+                    {
+                        // get the date of the file
+                        FileInfo info = new FileInfo(f);
+
+                        if (string.IsNullOrEmpty(sourceFile) || sourceFileDate < info.LastWriteTime)
+                        {
+                            sourceFile = f;
+                            sourceFileDate = info.LastWriteTime;
+                        }
+                    }
+                    else if (source != f)
+                    {
+                        string path = f.Remove(0, Directory.GetCurrentDirectory().Length);
+
+                        Console.WriteLine("OVERWRITING: " + path);
+                        File.Copy(source, f, true);
+                    }
+                }
+
+                SearchForSourceFile(fileNameToFind, d, source);
+            }
+        }
+    }
+}
