005f12f9628fba7758d0a4dff0b02327e3e7debd
ftrocchi <ftrocchi@yahoo.com>
2015-04-07 15:07:36 -0400

    More changes for servos

--- Source Code/Arduino/I2C_Servo/DomePanels.h
+++ Source Code/Arduino/I2C_Servo/DomePanels.h
@@ -3,9 +3,19 @@
 
 #if SERVOBOARD == 1
 
+#define SERVOCOUNT 10
+
 I2C_DeviceAddress::Value servoBoardAddress = I2C_DeviceAddress::DomePanelServo;
+Servo servos[SERVOCOUNT];
+int servoMins[SERVOCOUNT];
+int servoMaxs[SERVOCOUNT];
 
 void setupServoBoard() {
+    for (int i=0; i<maxServosIndex; i++) {
+        servos[i].attach(i+2);
+        servoMins[i] = 0;
+        servoMaxs[i] = 180;
+    }
 }
 
 #endif
--- Source Code/Arduino/I2C_Servo/I2C_Servo.ino
+++ Source Code/Arduino/I2C_Servo/I2C_Servo.ino
@@ -1,9 +1,10 @@
 // There are (at this time) 3 servo boards expected to be in place
 // change this to set the correct settings for the servo boards
 // 1 = Dome Panels, 2 = Dome HPs, 3 = Body
-#define SERVOBOARD 2
+#define SERVOBOARD 1
 
 #include <Wire.h>
+#include <Servo.h>
 #include "DebugHelper.h"
 #include "I2C_Common.h"
 #include "DomePanels.h"
@@ -17,3 +18,20 @@ void setup() {
 
 void loop() {
 }
+
+int clamp(int value, int min, int max) {
+    if (value < min) 
+        return min;
+    else if (value > max)
+        return max;
+    else
+        return value;
+}
+
+void setServoPosition(int servo, int value) {
+    if (servo > maxServosIndex)
+        return;
+    
+    servos[servo].write(clamp(value, servoMins[servo], servoMaxs[servo]));
+}
+
